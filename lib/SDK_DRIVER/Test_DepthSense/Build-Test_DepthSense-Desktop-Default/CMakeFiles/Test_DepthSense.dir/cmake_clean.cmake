b1eb1b122db35418da3bcbe079871f066deeb5cf
djyamunaq <dy.xcvi@gmail.com>
2023-02-22 22:22:28 +0100

    test1

---
+++ lib/SDK_DRIVER/Test_DepthSense/Build-Test_DepthSense-Desktop-Default/CMakeFiles/Test_DepthSense.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Test_DepthSense.dir/src/main.cpp.o"
+  "Test_DepthSense.pdb"
+  "Test_DepthSense"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Test_DepthSense.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
